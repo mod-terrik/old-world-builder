afeef9c0868492514d2de97c6f03cc34dbe916f6
mod-terrik <nico@guerrera.me>
2026-02-23 01:24:29 +0000

    Local rules

--- src/components/rules-index/rules-map.js
+++ src/components/rules-index/rules-map.js
@@ -1,7 +1,7 @@
 import rulesIndexExport from "./rules-index-export.json";
 
 const additionalOWBRules = {
-  "gnoblar scraplauncher": { url: "https://owapps.grra.me/localrules/unit/gnoblar-scraplauncher" },
+  "gnoblar scraplauncher": { fullUrl: "https://owapps.grra.me/unit/gnoblar-scraplauncher.html" },
   "throwing spears": { url: "weapons-of-war/throwing-spear" },
   halberds: { url: "weapons-of-war/halberd" },
   "additional hand weapons": {
